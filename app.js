fccde3f338c86c33d583960531e4944aee60e489
Moulikath <badaroumoulikath872@gail.com>
2023-02-14 10:16:21 +0100

    Ajout des fichier

---
+++ app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    let x=document.getElementById('navbar');
+       if (x.className==="nav") {
+           x.className+=" responsive";
+       } else {
+           x.className="nav";
+           
+       }
+}
